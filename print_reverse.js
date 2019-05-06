22e92706fd14e555dbb3c189b4386e0373e120a2
Yichuan wang <44794108+wyc6990906@users.noreply.github.com>
2019-05-06 15:20:26 -0600

    step 6

---
+++ print_reverse.js
@@ -0,0 +1,13 @@
+var getHTML = require("./http-functions");
+
+var requestOptions = {
+  host: "sytantris.github.io",
+  path: "/http-examples/step6/reverse.html"
+};
+
+function printReverse(html) {
+  /* Write your code here! */
+  console.log([...html].reverse().join(""));
+}
+
+getHTML(requestOptions, printReverse);
---
+++ print_uppercase.js
@@ -0,0 +1,13 @@
+var getHTML = require("./http-functions");
+
+var requestOptions = {
+  host: "sytantris.github.io",
+  path: "/http-examples/step6/uppercase.html"
+};
+
+function printUpperCase(html) {
+  /* Write your code here! */
+  console.log(html.toUpperCase());
+}
+
+getHTML(requestOptions, printUpperCase);
